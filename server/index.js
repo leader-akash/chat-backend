bb0c73f9fd3fb6b7959173fa5695f5d077149c11
leader-akash <leader.akashak@gmail.com>
2024-05-24 17:52:17 +0530

    url changes

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const { app, server } = require('./socket/index')
 
 // const app = express()
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
+    origin : process.env.FRONTEND_URL || process.env.PRODUCTION_URL,
     credentials : true
 }))
 app.use(express.json())

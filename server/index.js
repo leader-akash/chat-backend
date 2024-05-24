18dc24d463847af3147c5885dd162ce3827dc89c
leader-akash <leader.akashak@gmail.com>
2024-05-24 18:33:24 +0530

    cors error

--- server/index.js
+++ server/index.js
@@ -9,12 +9,26 @@ const { app, server } = require('./socket/index')
 // const app = express()
 
 app.use(express.json())
+
 app.use(express.urlencoded({extended:true}))
+
 app.use(cors({
     origin : ["https://classy-chat-frontend.vercel.app"],
     methods: ["POST", "GET", "PUT"],
     credentials : true
 }))
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://classy-chat-frontend.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", "true");
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 app.use(cookiesParser())
 
 const PORT = process.env.PORT || 8080

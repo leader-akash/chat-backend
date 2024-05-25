75cf6d84571add132f60c9758b2ab6dc144ac7d8
leader.akash <leader.akashak@gmail.com>
2024-05-26 01:58:38 +0530

    working

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -17,7 +17,7 @@ const Home = () => {
   const fetchUserDetails = async()=>{
     try {
         const URL = `${process.env.REACT_APP_BACKEND_URL}/api/user-details`
-        const response = await axios.get({
+        const response = await axios({
           url : URL,
           withCredentials : true
         })

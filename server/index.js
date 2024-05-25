08fef635349d2d7d7c1b779980105469e6814fc0
leader.akash <leader.akashak@gmail.com>
2024-05-25 18:13:27 +0530

    solving cors error

--- server/index.js
+++ server/index.js
@@ -14,30 +14,31 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cors({
     origin: ["https://classy-chat-frontend.vercel.app"],
     methods: ["POST", "GET", "PUT"],
-    credentials: true
+    credentials: true,
+    allowedHeaders: 'Content-Type,Authorization',
 }))
 
-const allowCors = fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-        res.status(200).end()
-        return
-    }
-    return await fn(req, res)
-}
+// const allowCors = fn => async (req, res) => {
+//     res.setHeader('Access-Control-Allow-Credentials', true)
+//     res.setHeader('Access-Control-Allow-Origin', '*')
+//     // another common pattern
+//     // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+//     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+//     res.setHeader(
+//         'Access-Control-Allow-Headers',
+//         'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+//     )
+//     if (req.method === 'OPTIONS') {
+//         res.status(200).end()
+//         return
+//     }
+//     return await fn(req, res)
+// }
 
-// Apply allowCors as middleware
-app.use(allowCors((req, res, next) => {
-    next();
-}));
+// // Apply allowCors as middleware
+// app.use(allowCors((req, res, next) => {
+//     next();
+// }));
 
 app.use(cookiesParser())
 

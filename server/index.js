fe997e22e17b1652bb235f7363135a148d944d3a
leader-akash <leader.akashak@gmail.com>
2024-05-27 13:21:07 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: true }))
 //   })
 
 app.use(cors({
-    origin : process.env.FRONTEND_URL,
+    origin :[ process.env.FRONTEND_URL,'http://localhost:3000' ],
     credentials : true
 }))
 
